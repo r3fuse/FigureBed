1f41afeb4e68f54ff2d6b4fdc843e460012336bd
REC <recoverycc@qq.com>
2025-03-15 19:43:32 +0800

    服务端基本功能已完善

---
+++ index.js
@@ -0,0 +1,51 @@
+const express  = require('express')
+const cors = require('cors')
+const app = express()
+const uuid = require('node-uuid')
+const bodyParser = require('body-parser')
+const fs  = require('fs')
+const path = require('path')
+const port = 9999
+app.use(express.static('public'))
+app.use(cors())
+
+app.use(bodyParser.text({
+    type:'text/plain',
+    limit:'10MB'
+}))
+app.listen(port,()=>{
+    console.log('server is running!');
+})
+
+let uuidd;
+
+app.post('/',(req,res)=>{
+    // console.log(req.body);
+    console.log(JSON.parse(req.body).fileType);
+    
+    const {fileType,imgBase64} = JSON.parse(req.body)
+    console.log(imgBase64);
+    
+    // const encoder = new TextEncoder('utf-8');
+    // console.log(encoder.encode(req.body).length);
+    const imgUrl = base64ToImage(imgBase64)
+    res.send(uuidd+'.'+fileType)
+})
+
+
+
+const base64ToImage = (url)=>{
+    uuidd = uuid.v4()
+    const base64Data = url.replace(/^data:image\/\w+;base64,/, '')
+    const imgBuffer = Buffer.from(base64Data,'base64')
+    const outputFilePath = path.join(__dirname,`./public/${uuidd}.png`)
+    fs.writeFile(outputFilePath,imgBuffer,(err)=>{
+        if(err){
+            console.log("失败",err);
+            
+        }else{
+            console.log("成功",uuidd);
+            return uuidd
+        }
+    })
+}
